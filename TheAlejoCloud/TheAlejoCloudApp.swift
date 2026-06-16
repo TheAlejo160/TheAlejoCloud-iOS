fd913d6379e5659114d1e2a1ef88ec74582f59c0
Rodrigo Apcho <135272939+TheAlejo160@users.noreply.github.com>
2026-06-16 14:58:51 -0500

    Initial Commit

---
+++ TheAlejoCloud/TheAlejoCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheAlejoCloudApp.swift
+//  TheAlejoCloud
+//
+//  Created by Rodrigo Alejandro Apcho Aliaga on 16/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TheAlejoCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheAlejoCloudTests/TheAlejoCloudTests.swift
@@ -0,0 +1,16 @@
+//
+//  TheAlejoCloudTests.swift
+//  TheAlejoCloudTests
+//
+//  Created by Rodrigo Alejandro Apcho Aliaga on 16/06/26.
+//
+
+import Testing
+
+struct TheAlejoCloudTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
